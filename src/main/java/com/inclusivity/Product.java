bc1ea9c97dde5e6b05ac0c9a0e17adf1bf48019d
Philip Lourandos <philip.lourandos@gmail.com>
2019-10-05 08:55:06 +0200

    * Added product pojo

---
+++ src/main/java/com/inclusivity/Product.java
@@ -0,0 +1,18 @@
+package com.inclusivity;
+
+import java.math.BigDecimal;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Product {
+    private String name;
+
+    private BigDecimal price;
+
+}
